f9e6a5a1eb108c1dd836643caa5965c6a56eaa91
Umut Benzer <ub@ubenzer.com>
2012-12-29 22:25:49 +0200

    main now creates platform manager

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,37 +3,45 @@ package main;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
-import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
-import jade.wrapper.StaleProxyException;
+
+import java.awt.EventQueue;
+import java.io.File;
+
+import misc.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import view.PlatformManager;
 
 public class Main {
   
   public static void main(String args[]) {
+    // Initialize log
+    String path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+    PropertyConfigurator.configure(path + File.separator + "log4j.properties");
+    
+    Logger.info("Platform başlatılıyor...");
     
     Profile p = new ProfileImpl();
     p.setParameter(Profile.MAIN_HOST, "localhost");
     p.setParameter(Profile.MAIN_PORT, "3250");
     
     Runtime r = Runtime.instance();
-    ContainerController cc = r.createMainContainer(p);
-    if (cc != null) {
-      try {
-        AgentController ac = cc.createNewAgent("insan1", "agent.MusicSeeker", null);
-        ac.start();
-        
-        ac = cc.createNewAgent("SONY", "agent.MusicProvider", null);
-        ac.start();
-        
-        ac = cc.createNewAgent("EROL KÖSE", "agent.MusicProvider", null);
-        ac.start();
-        
-      } catch (StaleProxyException e) {
-        e.printStackTrace();
+    final ContainerController cc = r.createMainContainer(p);
+    
+    Logger.info("Platform yöneticisi arayüzü başlatma treadi yaratılıyor...");
+    EventQueue.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Logger.info("Platform yöneticisi arayüzü başlatılıyor...");
+          PlatformManager frame = new PlatformManager(cc);
+          frame.setVisible(true);
+        } catch (Exception e) {
+          Logger.error(e, "Platform yöneticisi arayüzü başlatılamadı.");
+        }
       }
-    }
-     
-
+    });
   }
-  
 }
