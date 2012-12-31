4983fe60e0542e30238d574386851317da90e8a2
Umut Benzer <ub@ubenzer.com>
2012-12-31 03:06:55 +0200

    better logger with ajax names included.

--- src/misc/Logger.java
+++ src/misc/Logger.java
@@ -1,148 +1,57 @@
 package misc;
 
+import jade.core.Agent;
 
 public class Logger {
 
   public static org.apache.log4j.Logger log4j = org.apache.log4j.Logger.getLogger("root");
 
-  /**
-   * Log with DEBUG level
-   * 
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
-  public static void debug(String message, Object... args) {
-    log4j.debug(format(message, args));
-  }
-
-  /**
-   * Log with DEBUG level
-   * 
-   * @param e
-   *          the exception to log
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
-  public static void debug(Throwable e, String message, Object... args) {
-    log4j.debug(format(message, args), e);
-  }
-
-  /**
-   * Log with INFO level
-   * 
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
   public static void info(String message, Object... args) {
-    log4j.info(format(message, args));
+    log4j.info(format(null, message, args));
   }
-
-  /**
-   * Log with INFO level
-   * 
-   * @param e
-   *          the exception to log
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
   public static void info(Throwable e, String message, Object... args) {
-    log4j.info(format(message, args), e);
+    log4j.info(format(null, message, args), e);
+  }
+  public static void info(Agent agent, String message, Object... args) {
+    log4j.info(format(agent, message, args));
+  }
+  public static void info(Agent agent, Throwable e, String message, Object... args) {
+    log4j.info(format(agent, message, args), e);
   }
 
-  /**
-   * Log with WARN level
-   * 
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
   public static void warn(String message, Object... args) {
-    log4j.warn(format(message, args));
+    log4j.warn(format(null, message, args));
   }
-
-  /**
-   * Log with WARN level
-   * 
-   * @param e
-   *          the exception to log
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
   public static void warn(Throwable e, String message, Object... args) {
-    log4j.warn(format(message, args), e);
+    log4j.warn(format(null, message, args), e);
+  }
+  public static void warn(Agent agent, Throwable e, String message, Object... args) {
+    log4j.warn(format(agent, message, args), e);
+  }
+  public static void warn(Agent agent, String message, Object... args) {
+    log4j.warn(format(agent, message, args));
   }
 
-  /**
-   * Log with ERROR level
-   * 
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
   public static void error(String message, Object... args) {
-    log4j.error(format(message, args));
+    log4j.error(format(null, message, args));
   }
-
-  /**
-   * Log with ERROR level
-   * 
-   * @param e
-   *          the exception to log
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
   public static void error(Throwable e, String message, Object... args) {
-    log4j.error(format(message, args), e);
+    log4j.error(format(null, message, args), e);
   }
-
-  /**
-   * Log with FATAL level
-   * 
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
-  public static void fatal(String message, Object... args) {
-    log4j.fatal(format(message, args));
+  public static void error(Agent agent, Throwable e, String message, Object... args) {
+    log4j.error(format(agent, message, args), e);
   }
-
-  /**
-   * Log with FATAL level
-   * 
-   * @param e
-   *          the exception to log
-   * @param message
-   *          The message pattern
-   * @param args
-   *          Pattern arguments
-   */
-  public static void fatal(Throwable e, String message, Object... args) {
-    log4j.fatal(format(message, args), e);
+  public static void error(Agent agent, String message, Object... args) {
+    log4j.error(format(agent, message, args));
   }
-
-  /**
-   * Try to format messages using java Formatter. Fall back to the plain message
-   * if error.
-   */
-  static String format(String msg, Object... args) {
+  
+  static String format(Agent agent, String msg, Object... args) {
     try {
       if (args != null && args.length > 0) {
         return String.format(msg, args);
+      }
+      if(agent != null) {
+        msg = "Agent: " + agent.getLocalName() + " - ";
       }
       return msg;
     } catch (Exception e) {
